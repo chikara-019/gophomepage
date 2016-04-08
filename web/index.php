49881a4a2bddad2fffe39a101cfc88325e30a8b2
hajime matsumoto <hajime.matsumoto@avap.co.jp>
2016-04-08 16:14:17 +0900

    x

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
      $action= $_GET["action"];
      $file_path = ACTION_DIR."/{$action}.php";
 
-     if(is_file($file_path)) {     
+     if(is_file($file_path)) {
          //ファイルが存在すれば何もしない
 
      } elseif($action == '') {
@@ -13,12 +13,11 @@
          //indexをアクションにする
          $action = 'index';
          $file_path = ACTION_DIR."/{$action}.php";
-     
+
      } else {
          //ファイルが存在しないでactionが空でもない場合はerrorページ
          $action = 'error';
          $file_path = ACTION_DIR."/{$action}.php";
-     
      }
      include($file_path);
      $action_function = $action."_action";
